000d48daf73a00b792648f5cedaec3d2ec901951
ruchaj21 <rucha.prime@gmail.com>
2026-04-23 17:24:38 +0530

    Add ColumnChooserController component with button

--- Controller.tsx
+++ Controller.tsx
@@ -1,1 +1,25 @@
 
+import React, { useState } from 'react';
+import { Button } from '@ubs.websdk/react-core';
+import { CustomColumnChooser } from './CustomColumnChooser'; 
+
+export const ColumnChooserController = ({ gridApi }: { gridApi: any }) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  return (
+    <>
+      <Button
+        className="gridheader colchooserbutton ml-2"
+        icon="settings-14"
+        size="optional"
+        onClick={() => setIsOpen(true)}
+        title="Column settings"
+      />
+      <CustomColumnChooser
+        isOpen={isOpen}
+        onClose={() => setIsOpen(false)}
+        gridApi={gridApi}
+      />
+    </>
+  );
+};
